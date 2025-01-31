88c164daeabc34c34715239a475c8890b107b1e7
Nitish Rajbhar <nitishrajbhar7045@gmail.com>
2025-02-01 03:45:38 +0530

    Added MongoDB connection URI

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("MongoDB connection error: ", err));
+
 app.get("/", (req, res) => {
-    res.send("Fantasy Sports Backend is Running! 🚀");
+  res.send("Fantasy Sports Backend is Running! 🚀");
 });
 
 const PORT = process.env.PORT || 5000;
